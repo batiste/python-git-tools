25d769708b573a15522662e5915f1772051435db
Batiste <batiste@batiste-laptop.(none)>
2009-09-25 14:04:49 +0200

    Add the script and the doc

---
+++ git.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import glob
+import sys
+import os
+syntax = """Syntax python git.py [commit|push|pull|status|new_package] \"[your message|package name]\""""
+
+if len(sys.argv) < 2 or len(sys.argv) > 3:
+    print syntax
+    exit(0)
+
+if sys.argv[1] not in ("commit", "push", "pull", "status", "diff"):
+    print syntax
+
+dir_msg = "--- In directory : %s"
+
+if sys.argv[1] == 'commit':
+    if len(sys.argv) < 3:
+        print "You need to provide a message for your commit"
+        print syntax
+        exit(0)
+    commit_cmd = "cd %s; git commit -a -m \"%s\"; cd .. "
+    for dirname in glob.glob('*'):
+        print dir_msg % dirname
+        os.system(commit_cmd % (dirname, sys.argv[2]))
+
+if sys.argv[1] == 'pull':
+    commit_cmd = "cd %s; git pull; cd .. "
+    for dirname in glob.glob('*'):
+        print dir_msg % dirname
+        os.system(commit_cmd % (dirname))
+
+if sys.argv[1] == 'push':
+    commit_cmd = "cd %s; git push; cd .. "
+    for dirname in glob.glob('*'):
+        print dir_msg % dirname
+        os.system(commit_cmd % (dirname))
+
+if sys.argv[1] == 'status':
+    commit_cmd = "cd %s; git status; cd .. "
+    for dirname in glob.glob('*'):
+        print dir_msg % dirname
+        os.system(commit_cmd % (dirname))
+
+
+if sys.argv[1] == 'diff':
+    commit_cmd = "cd %s; git diff; cd .. "
+    for dirname in glob.glob('*'):
+        if dirname.find('.') == -1:
+            print dir_msg % dirname
+            os.system(commit_cmd % (dirname))
+
+
+if sys.argv[1] == 'new_package':
+    import re
+    app = sys.argv[2]
+    found = False
+    for p in glob.os.listdir(app):
+        filename = os.path.join(app, p, '__init__.py')
+        if os.path.exists(filename):
+            found = filename
+            break
+    if not found:
+        print "__init__.py file not found"
+        exit(0)
+
+    fh = open(filename, 'r')
+    content = fh.read()
+    fh.close()
+    import re
+    version = r'VERSION = \((\d+), (\d+), (\d+)\)'
+    search_result = re.search(version, content)
+    if not search_result:
+        print "Regexp %s didn't match anything in file %s" % (version, filename)
+        exit(0)
+    minor_version_number = int(search_result.group(3)) + 1
+    new_version = 'VERSION = (%d , %d, %d)' % (
+        int(search_result.group(1)),
+        int(search_result.group(2)),
+        minor_version_number
+    )
+    new_content = re.sub(version, new_version, content)
+    fh = open(filename, 'w')
+    fh.write(new_content)
+    fh.close()
+    print "Version number increased; commit changes"
+    commit_cmd = "cd %s; git commit -a -m \"New %s\"; git push; cd .." % (app, new_version.lower())
+    os.system(commit_cmd)
+    print "Upload the new package"
+    upload_cmd = "cd %s; python setup.py sdist upload -r chishop; cd .." % (app)
+    os.system(upload_cmd)
