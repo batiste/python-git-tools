d70b85f18c03926b2fbcaad62213c2894be87ff8
Batiste Bieler <batisteb@opera.com>
2010-06-08 14:01:37 +0200

    Fix a bugs

--- git.py
+++ git.py
@@ -77,7 +77,7 @@ def list_repos():
 
 def find_distmeta_file(app):
     for p in os.listdir(app):
-        if p not in ('testproj', 'example', 'tests'):
+        if p not in ('testproj', 'example', 'tests', 'test'):
             filename = os.path.join(app, p, 'distmeta.py')
             if os.path.exists(filename):
                 return filename
